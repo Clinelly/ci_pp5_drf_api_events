02c4cdf2cfe4aafca4713ebfb2e3d1d783517f83
Clinelly <swall289@gmail.com>
2023-05-17 08:52:43 +0000

    Add image validation to event serializer

--- events/serializers.py
+++ events/serializers.py
@@ -8,6 +8,21 @@ class EventSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 * 2:
+            raise serializers.ValidationError(
+                'Image size too large. Must be 2MB or less.'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image too wide. Must be 4096px wide or less. '
+            )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image too tall. Must be 4096px tall or less. '
+            )
+        return value
+
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
