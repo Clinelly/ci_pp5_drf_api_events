5addc270f20c47ee2cb01ad6a71dc6386dba8033
Clinelly <swall289@gmail.com>
2023-05-17 15:23:06 +0000

    Add validation error to like serializer

--- likes/serializers.py
+++ likes/serializers.py
@@ -1,3 +1,4 @@
+from django.bd import IntegrityError
 from rest_framework import serializers
 from .models import Like
 
@@ -10,3 +11,11 @@ class LikeSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'created_at', 'owner', 'event'
         ]
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'You cannot like the same event more than once.'
+                })
