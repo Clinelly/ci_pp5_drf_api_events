2da51b249703b06d1ea531c5e405f444d61ab979
Clinelly <swall289@gmail.com>
2023-05-17 15:48:02 +0000

    Add like views and urls

--- likes/serializers.py
+++ likes/serializers.py
@@ -1,21 +1,23 @@
-from django.bd import IntegrityError
+from django.db import IntegrityError
 from rest_framework import serializers
-from .models import Like
+from likes.models import Like
 
 
 class LikeSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Like model
+    The create method handles the unique constraint on 'owner' and 'event'
+    """
     owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
-        Model = Like
-        fields = [
-            'id', 'created_at', 'owner', 'event'
-        ]
+        model = Like
+        fields = ['id', 'created_at', 'owner', 'event']
 
     def create(self, validated_data):
         try:
             return super().create(validated_data)
         except IntegrityError:
             raise serializers.ValidationError({
-                'detail': 'You cannot like the same event more than once.'
-                })
+                'detail': 'You cannot like the same object more than once.'
+            })
