fdd31c53c638767aafb3f578d5813988dc7abaf3
Clinelly <swall289@gmail.com>
2023-05-18 10:46:38 +0000

    Add attending app and model

---
+++ attending/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class Attending(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(
+        Event, related_name='attending', on_delete=models.CASCADE
+        )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['owner', 'event']
+
+    def __str__(self):
+        return f'{self.owner} {self.event}'
