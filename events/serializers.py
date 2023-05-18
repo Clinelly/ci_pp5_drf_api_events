2232c53c16ceacf285ad0a0c483a8845ed38ca08
Clinelly <swall289@gmail.com>
2023-05-18 13:22:34 +0000

    Add like/attending fields to profile/event serializers

--- events/serializers.py
+++ events/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import Event
+from likes.models import Like
+from attending.models import Attending
 
 
 class EventSerializer(serializers.ModelSerializer):
@@ -7,6 +9,8 @@ class EventSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    like_id = serializers.SerializerMethodField()
+    attending_id = serializers.SerializerMethodField()
 
     def validate_image(self, value):
         if value.size > 1024 * 1024 * 2:
@@ -27,10 +31,29 @@ class EventSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                owner=user, event=obj
+            ).first()
+            return like.id if like else None
+        return None
+
+    def get_attending_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            attending = Attending.objects.filter(
+                owner=user, event=obj
+            ).first()
+            return attending.id if attending else None
+        return None
+
     class Meta:
         model = Event
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'title',
             'description', 'location', 'start_time', 'end_time', 'image',
-            'is_owner', 'profile_id', 'profile_image'
+            'is_owner', 'profile_id', 'profile_image', 'like_id',
+            'attending_id'
         ]
