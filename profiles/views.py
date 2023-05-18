27c2e7fe0522a626b9bfb8fdf2adc7e0b699f172
Clinelly <swall289@gmail.com>
2023-05-18 13:48:52 +0000

    Add extra data fields to profile views

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,8 @@
-from rest_framework import generics
+from rest_framework import generics, filters
 from events_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
+from django.db.models import Count
 
 
 class ProfileList(generics.ListAPIView):
@@ -9,8 +10,26 @@ class ProfileList(generics.ListAPIView):
     List all profiles.
     No create view as profile creation is handled by django signals.
     """
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        events_count=Count('owner__event', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True),
+        attending_count=Count('owner__attending', distinct=True),
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'events_count',
+        'followers_count',
+        'following_count',
+        'attending_count',
+        'owner__following__created_at',
+        'owner__followed__created_at',
+        'owner__attending__created_at',
+
+    ]
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
@@ -18,5 +37,10 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
     Retrieve or update a profile if you're the owner.
     """
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        events_count=Count('owner__event', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True),
+        attending_count=Count('owner__attending', distinct=True),
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
