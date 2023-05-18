d14c5652292be8cb8c12dcaa4973c9baf3554609
Clinelly <swall289@gmail.com>
2023-05-18 12:51:53 +0000

    Add refactoring of event and profile views

--- comments/views.py
+++ comments/views.py
@@ -1,7 +1,4 @@
-from django.http import Http404
-from rest_framework import status, generics, permissions
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics, permissions
 from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
 from events_api.permissions import IsOwnerOrReadOnly
--- events/views.py
+++ events/views.py
@@ -1,7 +1,4 @@
-from django.http import Http404
-from rest_framework import status, permissions
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics, permissions
 from .models import Event
 from .serializers import EventSerializer
 from events_api.permissions import IsOwnerOrReadOnly
@@ -9,7 +6,7 @@ from events_api.permissions import IsOwnerOrReadOnly
 # Create your views here.
 
 
-class EventList(APIView):
+class EventList(generics.ListCreateAPIView):
     """
     Lists all events.
     """
@@ -17,56 +14,10 @@ class EventList(APIView):
     permission_classes = [
         permissions.IsAuthenticatedOrReadOnly
     ]
-
-    def get(self, request):
-        events = Event.objects.all()
-        serializer = EventSerializer(
-            events, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = EventSerializer(
-            data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Event.objects.all()
 
 
-class EventDetail(APIView):
+class EventDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = EventSerializer
     permission_classes = [IsOwnerOrReadOnly]
-
-    def get_object(self, pk):
-        try:
-            event = Event.objects.get(pk=pk)
-            self.check_object_permissions(self.request, event)
-            return event
-        except Event.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        event = self.get_object(pk)
-        serializer = EventSerializer(
-            event, context={'request': request}
-            )
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        event = self.get_object(pk)
-        serializer = EventSerializer(
-            event, data=request.data, context={'request': request}
-            )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        event = self.get_object(pk)
-        event.delete()
-        return Response(
-            status=status.HTTP_204_NO_CONTENT
-        )
+    queryset = Event.objects.all()
