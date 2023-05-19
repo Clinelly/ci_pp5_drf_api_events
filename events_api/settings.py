a4d7d7b8c0b8c8f08b2194a8e7311d34b6ba3246
Clinelly <swall289@gmail.com>
2023-05-19 14:48:51 +0000

    Set up client origins and local hosts

--- events_api/settings.py
+++ events_api/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import re
 import os
 import dj_database_url
 from pathlib import Path
@@ -63,7 +64,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-clinelly-cipp5drfapieve-vusmxe0lr87.ws-eu97.gitpod.io', 'localhost', 'clinelly-ci-pp5-events-api.herokuapp.com']
+ALLOWED_HOSTS = [
+    '8000-clinelly-cipp5drfapieve-vusmxe0lr87.ws-eu97.gitpod.io',
+    'localhost',
+    os.environ.get('ALLOWED_HOST'),
+    ]
 
 # Application definition
 
@@ -108,9 +113,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN')
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 else:
     CORS_ALLOWED_ORIGIN_REGEXES = [
