b476dbe9e2ca3acce88214b4632078a005920991
Clinelly <swall289@gmail.com>
2023-06-13 08:05:28 +0000

    Add filterset fields to events views

--- events/views.py
+++ events/views.py
@@ -3,6 +3,7 @@ from .models import Event
 from .serializers import EventSerializer
 from events_api.permissions import IsOwnerOrReadOnly
 from django.db.models import Count
+from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
 
@@ -22,7 +23,14 @@ class EventList(generics.ListCreateAPIView):
     ).order_by('-created_at')
     filter_backends = [
         filters.OrderingFilter,
-        filters.SearchFilter
+        filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'owner__followed__owner__profile',
+        'likes__owner__profile',
+        'owner__profile',
+        'attending__owner__profile',
     ]
     search_fields = [
         "owner__username",
